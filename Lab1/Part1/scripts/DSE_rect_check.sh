28679bbe03077cfc3eeb3ce10aadc837a08aa537
Leonidas Avdelas <avdelasleonidas@gmail.com>
2020-01-11 14:27:08 +0200

    Results from rectangle

--- Lab1/Part1/scripts/DSE_rect_check.sh
+++ Lab1/Part1/scripts/DSE_rect_check.sh
@@ -9,12 +9,12 @@ echo "$executable" results > results/"$executable".txt
 
 for j in "${Bx[@]}"; do
     for k in "${By[@]}"; do 
+        echo bx: "$j" by "$k"
         for i in {1..10}; do 
             ./"$executable" "$j" "$k" >> _test.txt
         done
         avg=$(python scripts/statistics.py _test.txt avg)
         echo Bx: "$j", By: "$k" Average Time: "$avg" >> results/"$executable".txt
-
+    done
     rm -f _test.txt
 done
-
