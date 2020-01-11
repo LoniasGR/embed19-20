929f466c3519504c40003cdc2b971bc54fe2cea1
Leonidas Avdelas <avdelasleonidas@gmail.com>
2020-01-11 17:26:25 +0200

    More data

--- Lab1/Part1/scripts/run_tests.sh
+++ Lab1/Part1/scripts/run_tests.sh
@@ -2,11 +2,11 @@
 
 file="$2"
 executable="$1"
-B="$3"
-
+B1="$3"
+B2="$4"
 rm -rf "$file"
 
 for i in {1..10}
 do 
-    ./"$executable" "$B"  >> "$file"
+    ./"$executable" "$B1" "$B2"  >> "$file"
 done
