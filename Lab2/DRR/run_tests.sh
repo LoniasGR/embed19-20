9a44af1ea1a077ae0e61c115ae4e0163749325d6
Leonidas Avdelas <avdelasleonidas@gmail.com>
2020-01-16 17:53:55 +0200

    Lab2 run

---
+++ Lab2/DRR/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+execs = ('drr_SLLcl_SLLpk' 'drr_DLLcl_SLLpk' 'drr_SLLcl_DLLpk' \
+    'drr_DLLcl_DLLpk' 'drr_DYNARRcl_DLLpk' 'drr_DYNARRcl_SLLpk' \
+    'drr_DYNARRcl_DYNARRpk' 'drr_SLLcl_DYNARRpk' 'drr_DLLcl_DYNARRpk')
+
+for e in "${execs[@]}"; do
+    IFS='_'
+    read -ra NAME <<< "$e"
+    echo ${NAME[1]} ${name[2]}
+    valgrind --log-file="mem_accesses_log.txt" \
+        --tool=lackey \
+        --trace-mem=yes \
+        ./"$e"
+    cat mem_accesses_log.txt | grep 'I\| L' | wc -l
+    valgrind --tool=massif --massif-out-file=./test.out "$e"
+    ms_print ./test.out > mem_footprint_log_"$e".txt
+
+done
