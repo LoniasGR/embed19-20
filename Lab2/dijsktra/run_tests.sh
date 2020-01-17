bbd2b790a9c1dcc5c8e3295453af1b357c393184
Leonidas Avdelas <avdelasleonidas@gmail.com>
2020-01-17 16:36:13 +0200

    Scripts and makefile added

---
+++ Lab2/dijsktra/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+execs=("dijkstra_SLL" "dijkstra_DLL" "dijkstra_DYNARR" "dijkstra" )
+
+for e in "${execs[@]}"; do
+    IFS='_'
+    read -ra NAME <<< "$e"
+    echo ${NAME[1]}
+    valgrind --log-file="mem_accesses_log.txt" \
+        --tool=lackey \
+        --trace-mem=yes \
+        ./"$e"
+    cat mem_accesses_log.txt | grep 'I\| L' | wc -l
+    valgrind --tool=massif --massif-out-file=./test.out ./"$e"
+    ms_print ./test.out > results/mem_footprint_log_"${NAME[1]}.txt
+    rm test.out
+    rm mem_accesses_log.txt
+
+done
